5582bfb697e901c5a3b40d1d2217ceed180a5964
lalaalal <100madalala@gmail.com>
2018-10-15 00:38:13 +0900

    .

--- comci.py
+++ comci.py
@@ -12,12 +12,11 @@ json_str = res.text.split('\r')[0]
 학년 = 1
 반 = 7
 
-n요일 = ['월', '화', '수', '목', '금']
-
+n요일 = [None] * 5
 for 요일 in range(1, 6):
-    print(n요일[요일 - 1])
-
-    for 교시 in range(1, 6):
+    print(요일)
+    n교시 = [None] * 7
+    for 교시 in range(1, 8):
         원자료 = 자료["자료81"][학년][반][요일][교시]
         일일자료 = 자료["자료14"][학년][반][요일][교시]
 
@@ -27,5 +26,10 @@ for 요일 in range(1, 6):
         선생님 = 자료["자료46"][선생님_num]
         과목 = 자료["자료92"][과목_num]
 
-        print("선생님 : " + 선생님)
-        print("과목 : " + 과목)
+        n교시[교시 - 1] = [과목, 선생님]
+    n요일[요일 - 1] = n교시
+
+comci = { "요일" : n요일 }
+
+file = open("comci.json", "wt")
+json.dump(comci, file, ensure_ascii=False)
